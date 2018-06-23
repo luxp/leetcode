357253ed4bc73771450eb90836f1d033179bbc67
luxp <luxianpo@gmail.com>
2018-06-23 12:09:42 +0800

    add

---
+++ problems/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  let len = s.length;
+  let maxLen = 0;
+  let charStartDic = {};
+  let start = -1;
+  for (let i = 0; i < len; ++i) {
+    let c = s[i];
+    if (charStartDic[c] > start) {
+      //有重复
+      start = charStartDic[c];
+    }
+    maxLen = Math.max(i - start, maxLen);
+    charStartDic[c] = i;
+  }
+  return maxLen;
+};
+
+console.log(lengthOfLongestSubstring("cdes"));
