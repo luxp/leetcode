c1cb1ccc453fa85975a366693f97864a2242d98a
luxp <luxianpo@gmail.com>
2019-02-19 22:40:16 +0800

    style

--- src/123.best-time-to-buy-and-sell-stock-iii.js
+++ src/123.best-time-to-buy-and-sell-stock-iii.js
@@ -13,26 +13,26 @@
  *
  * Say you have an array for which the ith element is the price of a given
  * stock on day i.
- * 
+ *
  * Design an algorithm to find the maximum profit. You may complete at most two
- * transactions.          
- * 
+ * transactions.
+ *
  * Note: You may not engage in multiple transactions at the same time (i.e.,
  * you must sell the stock before you buy again).
- * 
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: [3,3,5,0,0,3,1,4]
  * Output: 6
  * Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit
  * = 3-0 = 3.
  * Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 =
  * 3.
- * 
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: [1,2,3,4,5]
  * Output: 4
  * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit
@@ -41,15 +41,15 @@
  * are
  * engaging multiple transactions at the same time. You must sell before buying
  * again.
- * 
- * 
+ *
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: [7,6,4,3,1]
  * Output: 0
  * Explanation: In this case, no transaction is done, i.e. max profit = 0.
- * 
+ *
  */
 /**
  * @param {number[]} prices
@@ -66,10 +66,10 @@ var maxProfit = function(prices) {
       }
     } else {
       let leftProfit = getMaxProfit(prices.slice(0, i))
-      let rightProfit =  getMaxProfit(prices.slice(i, len))
+      let rightProfit = getMaxProfit(prices.slice(i, len))
       profit = leftProfit + rightProfit
       if (rightProfit === 0 && prices[i - 1] >= prices[i]) {
-        return Math.max(maxValue, profit)        
+        return Math.max(maxValue, profit)
       }
     }
     if (profit > maxValue) {
@@ -77,9 +77,9 @@ var maxProfit = function(prices) {
     }
   }
   return maxValue
-};
+}
 
-function getMaxProfit (prices) {
+function getMaxProfit(prices) {
   let maxProfit = 0
   let len = prices.length
   let leftMin = prices[0]
@@ -100,14 +100,14 @@ function getMaxProfit (prices) {
 }
 
 test('getMaxProfit', () => {
-  expect(getMaxProfit([3,3,5,0,0])).toBe(2)
-  expect(getMaxProfit([1,2,3])).toBe(2)
+  expect(getMaxProfit([3, 3, 5, 0, 0])).toBe(2)
+  expect(getMaxProfit([1, 2, 3])).toBe(2)
 })
 
 test('maxProfit', () => {
-  expect(maxProfit([4,1,2])).toBe(1)
-  expect(maxProfit([1,2])).toBe(1)
-  expect(maxProfit([7,6,4,3,1])).toBe(0)
-  expect(maxProfit([1,2,3,4,5])).toBe(4)
-  expect(maxProfit([3,3,5,0,0,3,1,4])).toBe(6)
+  expect(maxProfit([4, 1, 2])).toBe(1)
+  expect(maxProfit([1, 2])).toBe(1)
+  expect(maxProfit([7, 6, 4, 3, 1])).toBe(0)
+  expect(maxProfit([1, 2, 3, 4, 5])).toBe(4)
+  expect(maxProfit([3, 3, 5, 0, 0, 3, 1, 4])).toBe(6)
 })
