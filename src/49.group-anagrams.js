6751bc7e80739c5b75e3926dbb8d589eaff8f12f
luxp <luxianpo@gmail.com>
2019-03-08 01:00:32 +0800

    add 49

---
+++ src/49.group-anagrams.js
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=49 lang=javascript
+ *
+ * [49] Group Anagrams
+ *
+ * https://leetcode.com/problems/group-anagrams/description/
+ *
+ * algorithms
+ * Medium (44.46%)
+ * Total Accepted:    299.4K
+ * Total Submissions: 667.2K
+ * Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+ *
+ * Given an array of strings, group anagrams together.
+ *
+ * Example:
+ *
+ *
+ * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Output:
+ * [
+ * ⁠ ["ate","eat","tea"],
+ * ⁠ ["nat","tan"],
+ * ⁠ ["bat"]
+ * ]
+ *
+ * Note:
+ *
+ *
+ * All inputs will be in lowercase.
+ * The order of your output does not matter.
+ *
+ *
+ */
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+  let recorder = {}
+  strs.forEach(str => {
+    let sortedStr = str
+      .split('')
+      .sort()
+      .join('')
+    recorder[sortedStr] = recorder[sortedStr] || []
+    recorder[sortedStr].push(str)
+  })
+  return Object.values(recorder)
+}
