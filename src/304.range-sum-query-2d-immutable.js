1e4ea0e1ff95660e2f4258b9535c341bcf199d1d
luxp <luxianpo@gmail.com>
2019-03-12 01:02:57 +0800

    add 304

---
+++ src/304.range-sum-query-2d-immutable.js
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=304 lang=javascript
+ *
+ * [304] Range Sum Query 2D - Immutable
+ *
+ * https://leetcode.com/problems/range-sum-query-2d-immutable/description/
+ *
+ * algorithms
+ * Medium (31.01%)
+ * Total Accepted:    64K
+ * Total Submissions: 204.3K
+ * Testcase Example:  '["NumMatrix","sumRegion","sumRegion","sumRegion"]\n[[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]'
+ *
+ * Given a 2D matrix matrix, find the sum of the elements inside the rectangle
+ * defined by its upper left corner (row1, col1) and lower right corner (row2,
+ * col2).
+ *
+ *
+ *
+ * The above rectangle (with the red border) is defined by (row1, col1) = (2,
+ * 1) and (row2, col2) = (4, 3), which contains sum = 8.
+ *
+ *
+ * Example:
+ *
+ * Given matrix = [
+ * ⁠ [3, 0, 1, 4, 2],
+ * ⁠ [5, 6, 3, 2, 1],
+ * ⁠ [1, 2, 0, 1, 5],
+ * ⁠ [4, 1, 0, 1, 7],
+ * ⁠ [1, 0, 3, 0, 5]
+ * ]
+ *
+ * sumRegion(2, 1, 4, 3) -> 8
+ * sumRegion(1, 1, 2, 2) -> 11
+ * sumRegion(1, 2, 2, 4) -> 12
+ *
+ *
+ *
+ * Note:
+ *
+ * You may assume that the matrix does not change.
+ * There are many calls to sumRegion function.
+ * You may assume that row1 ≤ row2 and col1 ≤ col2.
+ *
+ *
+ */
+/**
+ * @param {number[][]} matrix
+ */
+var NumMatrix = function(matrix) {
+  this.matrix = matrix
+}
+
+/**
+ * @param {number} row1
+ * @param {number} col1
+ * @param {number} row2
+ * @param {number} col2
+ * @return {number}
+ */
+NumMatrix.prototype.sumRegion = function(row1, col1, row2, col2) {
+  let result = 0
+  for (let i = row1; i <= row2; ++i) {
+    for (let j = col1; j <= col2; ++j) {
+      result += this.matrix[i][j]
+    }
+  }
+  return result
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = Object.create(NumMatrix).createNew(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
+
+test('default', () => {
+  var obj = new NumMatrix([
+    [3, 0, 1, 4, 2],
+    [5, 6, 3, 2, 1],
+    [1, 2, 0, 1, 5],
+    [4, 1, 0, 1, 7],
+    [1, 0, 3, 0, 5]
+  ])
+  expect(obj.sumRegion(2, 1, 4, 3)).toBe(8)
+})
