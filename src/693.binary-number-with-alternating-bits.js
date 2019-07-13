788d045fc11ca363ad74e9dbdff0db846b464eca
luxp <luxianpo@gmail.com>
2019-07-13 22:58:24 +0800

    ac 693

---
+++ src/693.binary-number-with-alternating-bits.js
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode id=693 lang=javascript
+ *
+ * [693] Binary Number with Alternating Bits
+ *
+ * https://leetcode.com/problems/binary-number-with-alternating-bits/description/
+ *
+ * algorithms
+ * Easy (58.09%)
+ * Total Accepted:    43.4K
+ * Total Submissions: 74.7K
+ * Testcase Example:  '5'
+ *
+ * Given a positive integer, check whether it has alternating bits: namely, if
+ * two adjacent bits will always have different values.
+ *
+ * Example 1:
+ *
+ * Input: 5
+ * Output: True
+ * Explanation:
+ * The binary representation of 5 is: 101
+ *
+ *
+ *
+ * Example 2:
+ *
+ * Input: 7
+ * Output: False
+ * Explanation:
+ * The binary representation of 7 is: 111.
+ *
+ *
+ *
+ * Example 3:
+ *
+ * Input: 11
+ * Output: False
+ * Explanation:
+ * The binary representation of 11 is: 1011.
+ *
+ *
+ *
+ * Example 4:
+ *
+ * Input: 10
+ * Output: True
+ * Explanation:
+ * The binary representation of 10 is: 1010.
+ *
+ *
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var hasAlternatingBits = function(n) {
+  let binaryStr = n.toString(2)
+  if (binaryStr.includes('11') || binaryStr.includes('00')) {
+    return false
+  }
+  return true
+}
+
+test('binary', () => {
+  expect(hasAlternatingBits(5)).toEqual(true)
+  expect(hasAlternatingBits(7)).toEqual(false)
+})
