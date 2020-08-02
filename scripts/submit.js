5eb20b72742915baff29aff4526c63fc64288503
luxp <luxianpo@gmail.com>
2020-08-02 15:28:43 +0800

    add auto open leetcode link for submit

--- scripts/submit.js
+++ scripts/submit.js
@@ -1,5 +1,8 @@
+const clipboardy = require('clipboardy')
 const fs = require('fs')
-const execSync = require('child_process').execSync
+const open = require('open')
+
+// const execSync = require('child_process').execSync
 const path = require('path')
 
 let fileName = process.argv[2]
@@ -8,7 +11,16 @@ let fileToSubmitContent = fs
   .toString()
 fileToSubmitContent += '\nfunction test() {}'
 
-let fileToSubmitPath = path.resolve(__dirname, '.temp.js')
-fs.writeFileSync(fileToSubmitPath, fileToSubmitContent)
-execSync(`leetcode submit ${fileToSubmitPath}`, { stdio: 'inherit' })
-execSync(`rm ${fileToSubmitPath}`)
+// let fileToSubmitPath = path.resolve(__dirname, '.temp.js')
+// leetcode cli can not login now issue: https://github.com/skygragon/leetcode-cli/issues/213
+// fs.writeFileSync(fileToSubmitPath, fileToSubmitContent)
+// execSync(`leetcode submit ${fileToSubmitPath}`, { stdio: 'inherit' })
+clipboardy.writeSync(fileToSubmitContent.toString())
+let leetcodeLink = getTheProblemLeetcodeLink(fileToSubmitContent)
+open(leetcodeLink)
+// execSync(`rm ${fileToSubmitPath}`)
+
+function getTheProblemLeetcodeLink(content) {
+  let matchResult = content.match(/https\:\/\/leetcode.com\/problems\/(.*?)\n/)
+  return matchResult[0]
+}
